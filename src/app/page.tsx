39644537df06918523c6e1c2c0ef187f5c7d87a3
gmr0780 <greg@thinksimple.com>
2026-03-22 15:00:13 -0400

    Mottley photo: shift down to center face

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -412,7 +412,7 @@ export default function Home() {
                         <img
                           src={s.image}
                           alt={s.name}
-                          className={`h-full w-full object-cover ${s.imageZoom ? "scale-[2] -translate-y-[15%]" : "object-top"}`}
+                          className={`h-full w-full object-cover ${s.imageZoom ? "scale-[2] translate-y-[10%]" : "object-top"}`}
                         />
                       ) : (
                         <span
